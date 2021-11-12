ca5da2bcf65d1f2081921db1a812a0ae33979ac7
spaceshiptrip <4379326+spaceshiptrip@users.noreply.github.com>
2021-11-11 21:17:07 -0800

    add API and code cleanup

--- src/components/OnLinePosts/GetOnLinePosts.js
+++ src/components/OnLinePosts/GetOnLinePosts.js
@@ -6,67 +6,65 @@ import CONFIG from '../../config.json';
 // get posts from online api
 // it's return a json file
 class GetOnlinePosts extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            error : null,
-            isLoaded : false,
-            posts : []
+            error: null,
+            isLoaded: false,
+            posts: []
         };
     }
 
-    componentDidMount(){
+    componentDidMount() {
         // I will use fake api from jsonplaceholder website
         // this return 100 posts
         // fetch("https://jsonplaceholder.typicode.com/posts")
-
-
         var headers = {}
-        let url = CONFIG.server + '/' + CONFIG.api;
+        let url = CONFIG.server + CONFIG.api;
 
         fetch(url, {
-          method: 'GET',
-          mode: 'cors', // no-cors, *cors, same-origin
-          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-          credentials: 'same-origin', // include, *same-origin, omit
-          headers: headers
+            method: 'GET',
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: headers
         })
-        .then( response => response.json())
-        .then(
-            // handle the result
-            (result) => {
-                this.setState({
-                    isLoaded : true,
-                    posts : result
-                });
-            },
+            .then(response => response.json())
+            .then(
+                // handle the result
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        posts: result
+                    });
+                },
 
-            // Handle error
-            (error) => {
-                this.setState({
-                    isLoaded: true,
-                    error
-                })
-            },
-        )
+                // Handle error
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    })
+                },
+            )
     }
 
     render() {
-        const {error, isLoaded, posts} = this.state;
+        const { error, isLoaded, posts } = this.state;
 
-        if(error){
+        if (error) {
             return <div>Error in loading</div>
-        }else if (!isLoaded) {
+        } else if (!isLoaded) {
             return <div>Loading ...</div>
-        }else{
-            return(
+        } else {
+            return (
                 <div>
-                   <Table data={posts}/>
+                    <Table data={posts} />
                 </div>
             );
         }
 
     }
-  }
+}
 
-  export default GetOnlinePosts;
+export default GetOnlinePosts;
