bbe272b5b502cf54dbc03f2c0cd0a280c77fcd71
Laney Franklin <Helena.C.Franklin@jpl.nasa.gov>
2022-11-01 11:18:05 -0700

    Update jest test so it wont fail hopefully

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders employee information link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Employee Information Table/i);
   expect(linkElement).toBeInTheDocument();
 });
